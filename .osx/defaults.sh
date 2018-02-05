f3ff6593983a9f8d15e9c17dbc267197b1068862
Izra Faturrahman <Frizz925@hotmail.com>
2018-02-05 15:17:27 +0700

    Removed .osx directory

--- .osx/defaults.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-defaults write .GlobalPreferences com.apple.mouse.scaling -1
-defaults write com.apple.screencapture disable-shadow -bool true
