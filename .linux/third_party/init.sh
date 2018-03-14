07735a856322fd3c70edfbcb08ae5718f6e03a1d
Izra Faturrahman <Frizz925@hotmail.com>
2018-03-15 01:45:45 +0700

    Update Linux and OSX init.sh git clone to run in parallel

--- .linux/third_party/init.sh
+++ .linux/third_party/init.sh
@@ -4,6 +4,6 @@ URLS=""
 URLS="${URLS} https://github.com/aaron-williamson/base16-gnome-terminal"
 
 for URL in ${URLS}; do
-	git clone ${URL} --depth=1
+	git clone ${URL} --depth=1 &&
 done
 
--- .osx/third_party/init.sh
+++ .osx/third_party/init.sh
@@ -4,6 +4,6 @@ URLS=""
 URLS="${URLS} https://github.com/chriskempson/base16-iterm2"
 
 for URL in ${URLS}; do
-	git clone ${URL} --depth=1
+	git clone ${URL} --depth=1 &
 done
 
