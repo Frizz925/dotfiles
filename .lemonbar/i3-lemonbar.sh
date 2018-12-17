ae254e00916fd98c4f21cdd736fe03e858f22827
Izra Faturrahman <Frizz925@hotmail.com>
2018-12-17 09:36:57 +0700

    Add unicode font

--- .lemonbar/i3-lemonbar.sh
+++ .lemonbar/i3-lemonbar.sh
@@ -6,9 +6,10 @@ GEOMETRY="x24"
 
 FONT="Roboto-8"
 FONT_ICON="FontAwesome-8"
+FONT_UNICODE="SourceHanSans-8"
 
 FOREGROUND="#D8DEE9"
 BACKGROUND="#2E3440"
 
 cd $(dirname $0)
-./i3-lemonbar-output.py | lemonbar -p -g "$GEOMETRY" -f "$FONT" -f "$FONT_ICON" -F "$FOREGROUND" -B "$BACKGROUND"
+./i3-lemonbar-output.py | lemonbar -p -g "$GEOMETRY" -f "$FONT" -f "$FONT_ICON" -f "$FONT_UNICODE" -F "$FOREGROUND" -B "$BACKGROUND"
