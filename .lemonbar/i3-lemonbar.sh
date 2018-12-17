9d9c68f24185792adc080eb00231d4cd7741be70
Izra Faturrahman <Frizz925@hotmail.com>
2018-12-17 17:44:22 +0700

    Add underline color

--- .lemonbar/i3-lemonbar.sh
+++ .lemonbar/i3-lemonbar.sh
@@ -3,6 +3,7 @@
 PWD=$(dirname $0)
 
 GEOMETRY="x24"
+UNDERLINE_WIDTH="2"
 
 FONT="Roboto-8"
 FONT_ICON="FontAwesome-8"
@@ -10,6 +11,7 @@ FONT_UNICODE="SourceHanSans-8"
 
 FOREGROUND="#D8DEE9"
 BACKGROUND="#2E3440"
+UNDERLINE="#D8DEE9"
 
 cd $(dirname $0)
-./i3-lemonbar-output.py | lemonbar -p -g "$GEOMETRY" -f "$FONT" -f "$FONT_ICON" -f "$FONT_UNICODE" -F "$FOREGROUND" -B "$BACKGROUND"
+./i3-lemonbar-output.py | lemonbar -p -g "$GEOMETRY" -f "$FONT" -f "$FONT_ICON" -f "$FONT_UNICODE" -F "$FOREGROUND" -B "$BACKGROUND" -U "$UNDERLINE" -u "$UNDERLINE_WIDTH"
