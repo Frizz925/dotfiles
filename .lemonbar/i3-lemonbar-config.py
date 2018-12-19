e0e7511afde72793fc50f4dcad4c016ba177a32c
Izra Faturrahman <Frizz925@hotmail.com>
2018-12-19 15:34:41 +0700

    Insert config from i3-lemonbar.sh

--- .lemonbar/i3-lemonbar-config.py
+++ .lemonbar/i3-lemonbar-config.py
@@ -1,6 +1,21 @@
 from customblocks import NowPlaying, Workspaces, Windows, PulseAudio
 from datetime import datetime
 
+BAR_HEIGHT = 22
+UNDERLINE_HEIGHT = 3
+
+FONT_SIZE = 8
+FONT_ICON_SIZE = 9
+
+FONT = 'Roboto'
+FONT_ICON = 'FontAwesome'
+FONT_UNICODE = 'SourceHanSans'
+FONT_EMOJI = 'NotoColorEmoji'
+
+FOREGROUND = "#D8DEE9"
+BACKGROUND = "#2E3440"
+UNDERLINE = "#D8DEE9"
+
 SEPARATOR = '   '
 
 LEFT_BLOCKS = [
