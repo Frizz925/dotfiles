6498a50cee097b4d05d899ec726f4c7a5fe6f3f3
Izra Faturrahman <Frizz925@hotmail.com>
2018-12-20 00:16:28 +0700

    Make block rendering more fault tolerant

--- .lemonbar/i3lemonbar/renderers/blocks_renderer.py
+++ .lemonbar/i3lemonbar/renderers/blocks_renderer.py
@@ -3,6 +3,8 @@ from ..blocks import Block
 from inspect import isclass
 from typing import List
 
+import codecs
+
 
 class BlocksRenderer(Renderer):
     def __init__(self, blocks: List[Block], separator='  '):
@@ -12,7 +14,9 @@ class BlocksRenderer(Renderer):
 
     def render(self) -> str:
         separator = self.render_separator()
-        return separator.join(map(self.render_block, self.blocks))
+        blocks = map(self.render_block, self.blocks)
+        blocks = filter(None, blocks)
+        return separator.join(blocks)
 
     def render_separator(self) -> str:
         return self.separator
@@ -20,7 +24,7 @@ class BlocksRenderer(Renderer):
     def render_block(self, block: Block) -> str:
         if block in self.render_map and not block.should_update():
             return self.render_map[block]
-        rendered = block.render() or ''
+        rendered = block.render()
         self.render_map[block] = rendered
         return rendered
 
@@ -33,4 +37,6 @@ class BlocksRenderer(Renderer):
             return block.render()
         if isinstance(block, str):
             return block
+        if isinstance(block, bytes):
+            return codecs.decode(block, 'utf-8')
         raise TypeError('Unknown block type of %s' % type(block))
